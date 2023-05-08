b21358e3dc41bb7ff9466d636889874e140510c5
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-05-08 10:19:56 +0100

    Copy from MS

--- R/data.R
+++ R/data.R
@@ -1,6 +1,18 @@
 #' Sibillini data
 #'
-#' TODO: Description of `Sibillini_pt` and `Sibillini_py`
+#' Geospatial polygon and point data sets capturing Italy's Monti
+#' 'Sibillini' National Park.
+#'
+#' The polygon data set ('Sibillini_py') contains two polygons dividing
+#' the park into regions of 'high' and 'low' altitude.
+#' The point data set ('Sibillini_pt') contains 100 points distributed randomly
+#' within the 'high' and 'low' sections.
+#'
+#' TODO: Complete description of `Sibillini_pt` and `Sibillini_py`
+#'
+#' @examples
+#' data("Sibillini_pt")
+#' #TODO: Do something with this data
 #'
 #' @keywords datasets
 #' @name Sibillini
