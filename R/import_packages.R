4d4453de22e793efec9640f932eecd8a6b565e79
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-05-08 10:30:59 +0100

    HEAD not used

--- R/import_packages.R
+++ R/import_packages.R
@@ -3,10 +3,8 @@
 #' Polygon Polygons SpatialPolygons
 #' @importFrom glue glue
 #' @importFrom terra rast extract vect crs ext intersect mosaic writeRaster
-#' @importFrom httr HEAD
 #' @importFrom geodata elevation_3s
 #' @importFrom elevatr get_elev_raster
 #' @importFrom fs file_temp
 #' @importFrom dplyr %>% group_by summarize
-utils::globalVariables(c("Name"))
 NULL
