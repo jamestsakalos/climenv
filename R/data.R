2a01dedfecdec17023eedeef3bdb8c8439dfbfe7
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-05-08 09:46:51 +0100

    Document data
    Placeholder for now

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' Sibillini data
+#'
+#' TODO: Description of `Sibillini_pt` and `Sibillini_py`
+#'
+#' @keywords datasets
+#' @name Sibillini
+NULL
+
+#' @rdname Sibillini
+"Sibillini_pt"
+
+#' @rdname Sibillini
+"Sibillini_py"
+
+#' Torricchio data
+#'
+#' TODO: Description of `Torricchio`
+#'
+#' @keywords datasets
+"Torricchio"
+
+#' STRM tile data
+#'
+#' TODO: Description of `srtm.tiles`
+#'
+#' @keywords datasets
+"srtm.tiles"
