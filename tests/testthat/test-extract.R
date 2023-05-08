442d414ed3b9bdb3a12e2f90e142de276b255c70
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-05-08 09:22:53 +0100

    Remove files on complete

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -1,6 +1,4 @@
-# Testing the ce_extract() function ####
-
-test_that("ce_extract works", {
+test_that("ce_extract() works", {
 
   # Set testing data ####
 
@@ -9,11 +7,13 @@ test_that("ce_extract works", {
 
   # Create the required subdirectories
   dir.create(file.path(temp_path, "elev"), recursive = TRUE)
+  on.exit(unlink(file.path(temp_path, "elev")))
 
   dir.create(file.path(temp_path, "clim/prec"), recursive = TRUE)
   dir.create(file.path(temp_path, "clim/tmax"), recursive = TRUE)
   dir.create(file.path(temp_path, "clim/tmean"), recursive = TRUE)
   dir.create(file.path(temp_path, "clim/tmin"), recursive = TRUE)
+  on.exit(unlink(file.path(temp_path, "clim")), add = TRUE)
 
   # Create a empty raster serving as a base
   r <- terra::rast(ncol = 10, nrow = 10)
