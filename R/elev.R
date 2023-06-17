4cd10222f4e6d9f4796c21620cb45414796a1093
James Tsakalos <jamestsakalos@gmail.com>
2023-06-17 10:02:31 +0200

    documentation and function updates to pass testing

--- R/elev.R
+++ R/elev.R
@@ -1,29 +1,28 @@
-# MS comment: Each documentation page should open with a brief informative title
-#' Download SRTM elevation data
+#' Download elevation data
 #'
 #' @description
-#' \code{elev()} downloads elevation data the Shuttle Radar Topography Mission
+#' `elev()` downloads elevation data the Shuttle Radar Topography Mission
 #' (SRTM) , specifically the hole-filled CGIAR-SRTM (90 m resolution) for
 #' latitudes between -60 and 60 or Mapzen's synthesis digital elevation product.
 #'
 #' @template output_dir_param
-#' @param location A \code{sp}, \code{sf} polygon or point object.
-#' @param source Character. Elevation sourced from mapzen (default) or geodata.
+#' @template output_loc_param
+#' @template output_e_source_param
 #'
 #' @return
-# MS: This is false; at present it returns `NULL`, the output of `rm()`.
-#' Returns one elevation tile (tiff). If elevation is sourced from geodata the
-#' elevation is downloaded at a spatial resolution of 30 arc seconds (~1 km2).
-#' If elevation data is sourced from mapzen then the product will be a mosaic.
-#' Specfically, Mapzen’s product is unique in that it combines several sources
-#' of digital elevation models, including SRTM, the ArcticDEM (covering all
-#' areas north of 60°), EUDEM (digital elevation model over Europe; for review,
-#' see Mouratidis & Ampatzidis, 2019), and others into a single product. The
-#' resolution of this product was set to 7, corresponding to 611.5&nbsp;m ground
-#' resolution at 60° latitude 864.8&nbsp;m at 45° and 1223&nbsp;m at 0°.
+#' Creates one subfolder named elev storing a raster (.tiff). If elevation is
+#' sourced from geodata the elevation is downloaded at a spatial resolution of
+#' 30 arc seconds (~1 km  sq.). If elevation data is from mapzen then the
+#' product will be a mosaic. Specifically, Mapzen’s product is unique as it
+#' combines several sources of digital elevation models, including SRTM, the
+#' ArcticDEM (covering all areas north of 60°), EUDEM (digital elevation model
+#' over Europe; for review, see Mouratidis & Ampatzidis, 2019), and others into
+#' a single product. The resolution of this product was set to 7, corresponding
+#' to 611.5 m ground resolution at 60° latitude 864.8 m at 45° and 1223 m at 0°.
 #'
 #' @author James L. Tsakalos
-#' @seealso Download climate data: [`ce_download()`]
+#' @seealso A more convenient function for other climate and elevation data
+#' [`ce_download()`].
 #' @references{ Hijmans, R.J., et al., (2023). geodata: Download Geographic
 #' Data. R package version 0.5-8. https://CRAN.R-project.org/package=geodata
 #'
@@ -40,24 +39,27 @@
 #' @examples
 #' \dontrun{
 #' # Start by loading Italy's Mount Sibillini National Park boundary
-#' data("Sibillini_py")
+#' data("Sibillini_py", package = "climenv")
 #' # elevation will be saved in the output_dir (i.e. output directory)
-#' elev(output_dir = "...Desktop/elev", location = Sibillini_py)
+#'    elev(output_dir = "...Desktop/elev", loc = Sibillini_py)
 #' }
 #' @importFrom elevatr get_elev_raster
 #' @importFrom geodata elevation_3s
 #' @importFrom sf as_Spatial
-#' @importFrom sp Polygon Polygons SpatialPolygons spTransform
-#' @importFrom terra crs ext intersect mosaic rast vect
+#' @importFrom sp Polygon Polygons SpatialPolygons
+#' @importFrom terra crs ext intersect mosaic rast vect project
 #' @export
-elev <- function(output_dir, location, source = "mapzen") {
+elev <- function(output_dir, loc, e_source = "mapzen") {
 
-  if (is.na(match(source, c("mapzen", "geodata"))))
-    stop("source must be one of mapzen or geodata")
+  if (is.na(match(e_source, c("mapzen", "geodata"))))
+    stop("e_source must be one of mapzen or geodata")
 
-  #Convert sf locations to SP
-  if (("sf" %in% class(location)) || ("sfc" %in% class(location))) {
-    location <- sf::as_Spatial(location)
+  # Read in the SRTM tiles
+  srtm_tiles <- climenv::srtm.tiles
+
+  # Convert sf locations to SP
+  if (("sf" %in% class(loc)) || ("sfc" %in% class(loc))) {
+    loc <- sf::as_Spatial(loc)
   }
 
   # Create elev folder
@@ -70,24 +72,18 @@ elev <- function(output_dir, location, source = "mapzen") {
                recursive = TRUE))
   }
 
-  # Read in the SRTM tiles
-  srtm_tiles <- climenv::srtm.tiles # preferable to data("srtm.tiles"); see ?data
-
-  # Set projection
-  location <- sp::spTransform(location, terra::crs(srtm_tiles))
-
   # Make a bounding box around the location
 
-  bbox <- matrix(terra::ext(terra::vect(location))[c(1, 3, 2, 4)], 2, 2)
+  bbox <- matrix(terra::ext(terra::vect(loc))[c(1, 3, 2, 4)], 2, 2)
   x <- c(bbox[1, 1], bbox[1, 1], bbox[1, 2], bbox[1, 2], bbox[1, 1])
   y <- c(bbox[2, 1], bbox[2, 2], bbox[2, 2], bbox[2, 1], bbox[2, 1])
   p <- sp::Polygon(cbind(x, y))
   ps <- sp::Polygons(list(p), "p1")
-  location <- sp::SpatialPolygons(list(ps), 1L,
+  loc <- sp::SpatialPolygons(list(ps), 1L,
                                   proj4string = terra::crs(srtm_tiles))
 
   # Intersect location and tiles
-  intersects <- terra::intersect(terra::vect(location), terra::vect(srtm_tiles))
+  intersects <- terra::intersect(terra::vect(loc), terra::vect(srtm_tiles))
   tiles <- srtm_tiles[srtm_tiles$FID %in% intersects$FID, ]
 
   # Download and merge the SRTM tiles
@@ -98,7 +94,7 @@ elev <- function(output_dir, location, source = "mapzen") {
 
     srtm_temp <- tempfile()
 
-    if (source == "geodata") {
+    if (e_source == "geodata") {
       # for  lat >= -60 & lat <= 60
 
       lon <- terra::ext(terra::vect(tiles[i, ]))[c(1)] +
@@ -117,7 +113,7 @@ elev <- function(output_dir, location, source = "mapzen") {
       )
     }
 
-    if (source == "mapzen") {
+    if (e_source == "mapzen") {
       tile <- terra::rast(
         elevatr::get_elev_raster(
           tiles[i, ], z = 7, override_size_check = TRUE,
@@ -143,6 +139,6 @@ elev <- function(output_dir, location, source = "mapzen") {
   terra::writeRaster(srtm_mosaic, filename = file_path, overwrite = TRUE)
 
   # Clean the elevation folder
-  rm(list = c("srtm_tiles"), envir = .GlobalEnv)
+  # rm(list = c("srtm_tiles"), envir = .GlobalEnv)
 
 }
