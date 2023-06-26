ff592ad1acef1995d5f2ca51527ee96a6616df70
James Tsakalos <jamestsakalos@gmail.com>
2023-06-26 17:21:24 +0300

    removing checks that are performed by another function. Updating documentation, adding ... so that other function arguments can be supplied.

--- R/chelsa.R
+++ R/chelsa.R
@@ -6,19 +6,19 @@
 #'  resolution of 30 arc sections.
 #'
 #' @template output_dir_param
-#' @template output_mode_param
-#' @template output_quiet_param
 #' @template output_var_param
+#' @param quiet,mode,\dots Arguments to control a download from the Internet
+#' `download.file()`.
 #'
 #' @return
 #' Returns four subfolders named prec, tmax, tmin and tmean. Each folder
 #' contains 12 GeoTiff (.tif) files, one for each month of the year for the time
 #' period 1981&ndash;2010. Each of the files are downloaded at a spatial
-#' resolution of 30 arc seconds (~1 km sq.). The precipitation folder contains
-#' average monthly precipitation (mm). The tmax folder contains maximum monthly
-#' temperature. The tmin folder contains minimum monthly temperature. The tmean
-#' folder contains the average monthly temperature. The unit of measure for
-#' temperature is in °C.
+#' resolution of 30 arc seconds (&sim;1 km sq.). The precipitation folder
+#' contains average monthly precipitation (mm). The tmax folder contains maximum
+#' monthly temperature. The tmin folder contains minimum monthly temperature.
+#' The tmean folder contains the average monthly temperature. The unit of
+#' measure for temperature is in &deg;C.
 #'
 #' @author James L. Tsakalos
 #' @seealso Downloading from WorldClim V2.1 [`worldclim()`] or a more convenient
@@ -38,14 +38,22 @@
 #'
 #' @examples
 #' \dontrun{
-#'    chelsa(output_dir = "...Desktop/Chelsa", quiet = FALSE)
+#'
+#' # Download time will depend on the size of the area you wish to access
+#' # climate data for and your internet connection speed.
+#'
+#' # Download the WorldClim data
+#' chelsa(
+#'   output_dir = "...Desktop/chelsa"
+#' )
+#'
+#' # Note that unlike worldclim() we do not specify the location argument
+#' # because it is not yet possible to extract smaller tile sections
+#'
 #' }
 #' @export
-chelsa <- function(output_dir = NULL, mode = "wb",
-                   quiet = FALSE, var = NULL) {
-
-  if (is.na(match(mode, c("w", "wb", "a", "ab"))))
-    stop("mode must be one of w, wb, a, ab.")
+chelsa <- function(output_dir = NULL, var = "all",
+                   mode = "wb", quiet = FALSE, ...) {
 
   if (is.null(output_dir))
     stop("Set output directory")
@@ -55,19 +63,22 @@ chelsa <- function(output_dir = NULL, mode = "wb",
   options(timeout = 1000)
 
   # I want to use these as a template, to apply correct names later.
-  var_t <- c("prec", "tmax", "tmin", "tmean")
+  var_t <- c("prec", "tmax", "tmin", "tavg")
   names(var_t) <- c("pr", "tasmin", "tasmax", "tas")
 
-  if (!is.null(var)) {
-
-    if (is.na(match(var, c("prec", "tmax", "tmin", "tmean"))))
-      stop("Valid options for `var` are prec, tmax, tmin, tmean.")
-
-    var <- var_t[var_t %in% var] # to assign the names
-
-  } else {
-    var <- c("prec", "tmax", "tmin", "tmean")
+  # Checks var, if an argument is provided it makes sure its correct.
+  # If var is null then it assigns all the values.
+  if (is.null(var) || var == "all") {
+    var <- c("prec", "tmax", "tmin", "tavg")
     names(var) <- c("pr", "tasmin", "tasmax", "tas")
+  } else {
+    stopifnot(var %in% c("prec", "tmax", "tmin", "tavg"))
+
+    # create clean names
+    var_c <- c("prec", "tmax", "tmin", "tavg")
+    names(var_c) <- c("pr", "tasmin", "tasmax", "tas")
+    var <- var_c[var_c %in% var]
+
   }
 
   # Make the folders
@@ -99,18 +110,13 @@ chelsa <- function(output_dir = NULL, mode = "wb",
         output_dir, "/", path_file, "/",  path_url, "_", layer, site_end
       )
 
-      # So that we do not download the file if it already exists
-      if (isFALSE(file.exists(file_path))) {
-        try(
-          download.file(
-            layer_url, file_path,
-            mode = mode, cacheOK = FALSE, quiet = quiet
-          )
+      # Download the file
+      try(
+        download.file(
+          layer_url, file_path,
+          mode = mode, quiet = quiet, ...
         )
-      } else {
-        print(paste0(file_path, " exists, please delete or specify new folder"))
-      }
-
+      )
     }
 
   }
