245925610124027e066e514e5c02d6238b25a792
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-07-07 16:55:59 +0100

    System files missing?

--- tests/testthat/test-worldclim.R
+++ tests/testthat/test-worldclim.R
@@ -39,9 +39,10 @@ test_that("worldclim() downloads data", {
   skip_if(!file.exists(tile_files[1]))
   jan50 <- terra::rast(tile_files[1])
   thumb <- terra::aggregate(jan50, fact = 64)
-  expected <- terra::rast(system.file("tests/testthat/expected/jan50.tif",
-                                      package = "climenv"))
-  expect_true(all.equal(rast(thumb), rast(expected)))
+  expectedFile <- system.file("tests/testthat/expected/jan50.tif",
+                              package = "climenv")
+  expect_false(expectedFile == "") # System file not found
+  expect_true(all.equal(rast(thumb), rast(rast(expectedFile))))
 
 
   # Run this code manually to update the "Expected" value
@@ -67,9 +68,10 @@ test_that("worldclim() downloads data", {
   # Check data matches expectation
   south_elev <- terra::rast(south_file)
   thumb <- terra::aggregate(south_elev, fact = 64)
-  expected <- terra::rast(system.file("tests/testthat/expected/south_elev.tif",
-                                      package = "climenv"))
-  expect_true(all.equal(rast(thumb), rast(expected)))
+  expected <- system.file("tests/testthat/expected/south_elev.tif",
+                                      package = "climenv")
+  expect_false(expected == "") # System file not found
+  expect_true(all.equal(rast(thumb), rast(rast(expected))))
 
 
   # Run this code manually to update the "Expected" value
