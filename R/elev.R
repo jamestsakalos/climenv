93e8370f86d34e3891effa5e34535128920aa30a
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-07-11 19:39:22 +0100

    <-

--- R/elev.R
+++ R/elev.R
@@ -118,7 +118,7 @@
 #' }
 #' @importFrom elevatr get_elev_raster
 #' @importFrom geodata elevation_3s
-#' @importFrom sf as_Spatial st_geometry st_bbox st_is_longlat st_crs
+#' @importFrom sf as_Spatial st_geometry st_bbox st_is_longlat st_crs<-
 #' @importFrom terra rast extract xyFromCell mosaic writeRaster rast
 #' @export
 elev <- function(output_dir, location, e_source = "mapzen") {
