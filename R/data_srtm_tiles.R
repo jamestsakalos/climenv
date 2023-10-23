ede815949e45e0c9fd4fde8dfcbf199043105dfc
SMITH <1695515+ms609@users.noreply.github.com>
2023-10-23 12:14:43 +0100

    Calrify description

--- R/data_srtm_tiles.R
+++ R/data_srtm_tiles.R
@@ -1,10 +1,11 @@
 ##' SRTM tiles data
 ##'
-##' SRTM tiles data.
+##' Polygons defining coordinates of Shuttle Radar Topography Mission data
+##' tiles.
 ##'
 ##' @docType data
-##' @format{ An object of class \code{SpatialPolygonsDataFrame} with 872 rows
-##' and 1 columns.}
+##' @format An object of class \code{SpatialPolygonsDataFrame} with 872 rows
+##' and 1 column.
 ##' @details Contains tiles to assist downloading from 'geodata'.
 ##' @encoding UTF-8
 ##' @keywords datasets
