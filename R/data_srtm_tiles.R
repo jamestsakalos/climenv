3ec77ba5aeb762c84a5c0feac873bc74d1a69798
SMITH <1695515+ms609@users.noreply.github.com>
2023-10-23 12:15:12 +0100

    Uncomment

--- R/data_srtm_tiles.R
+++ R/data_srtm_tiles.R
@@ -1,16 +1,16 @@
-##' SRTM tiles data
-##'
-##' Polygons defining coordinates of Shuttle Radar Topography Mission data
-##' tiles.
-##'
-##' @docType data
-##' @format An object of class \code{SpatialPolygonsDataFrame} with 872 rows
-##' and 1 column.
-##' @details Contains tiles to assist downloading from 'geodata'.
-##' @encoding UTF-8
-##' @keywords datasets
-##' @rdname srtm_tiles
-##' @examples
-##' data("srtm_tiles", package = "climenv")
-##' head(srtm_tiles)
+#' SRTM tiles data
+#'
+#' Polygons defining coordinates of Shuttle Radar Topography Mission data
+#' tiles.
+#'
+#' @docType data
+#' @format An object of class \code{SpatialPolygonsDataFrame} with 872 rows
+#' and 1 column.
+#' @details Contains tiles to assist downloading from 'geodata'.
+#' @encoding UTF-8
+#' @keywords datasets
+#' @rdname srtm_tiles
+#' @examples
+#' data("srtm_tiles", package = "climenv")
+#' head(srtm_tiles)
 "srtm_tiles"
